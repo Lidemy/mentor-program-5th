2ef4fefb336cfd809c1fd7a5a4f38e156ac2d4e4
cccc <xj3zjfan@gmail.com>
2021-04-15 16:27:20 +0800

    hw3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
+    let newStr ='';
+    for(let i= str.length-1; i>=0 ;i--){
+      newStr += str[i]
+    }
+    console.log(newStr)
+  }
   
-}
-
-reverse('hello');
+  reverse('hello');
+  
