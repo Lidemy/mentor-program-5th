31da38576e078aff63fd3c6059ca3bad02f7ff3a
cccc <xj3zjfan@gmail.com>
2021-04-15 16:38:39 +0800

    hw5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,18 @@
 function join(arr, concatStr) {
-  
+    let str = arr[0];
+    for(let i=1; i<arr.length; i++){
+      str+=concatStr;
+      str+=arr[i];
+    }
+    return str;
 }
-
-function repeat(str, times) {
   
+function repeat(str, times) {
+    let str1='';
+    for(let i=0 ; i<times ;i++){
+        str1+=str;
+    }
+    return str1;
 }
 
 console.log(join(['a'], '!'));
