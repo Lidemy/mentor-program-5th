6ff4f69333b599663d2ed61acf70e5b8fe636d95
Lu-yeom <82422161+Lu-yeom@users.noreply.github.com>
2021-04-25 19:23:07 +0800

    self-learning  by Luyeom-week_hw5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,25 @@
 function join(arr, concatStr) {
-  
+  var temp = ''
+    if (arr.length === 1) {
+     temp = arr[0] + concatStr
+    } else {
+      for (i = 1; i <arr.length; i++) {
+        if (i === arr.length -1) {
+          temp += arr[i]
+        } else {
+          temp = arr[i] + concatStr
+        }
+      }
+    }
+  return temp
 }
+console.log(join(['a'],'!'))
 
 function repeat(str, times) {
-  
+  var result = ''
+  for (i=1;i<=times;i++){
+    result += str
+  }
+  return result
 }
-
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(repeat('a', 5))
