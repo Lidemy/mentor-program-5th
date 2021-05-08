e3e4485b9c967d67eb0862a02e3e68314c64211e
Lu-yeom <82422161+Lu-yeom@users.noreply.github.com>
2021-05-08 22:17:19 +0800

    Update hw1.js

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -1,11 +1,18 @@
 const request = require('request');
-const process = require('process')
 
-console.log(process.argv)
-request(
+request.get(
   'https://lidemy-book-store.herokuapp.com/books?_limit=10',
   function (error,response,body) {
-    const json = JSON.parse(body)
-  console.log(json);
+    let data;
+    try {
+      data = JSON.parse(body);
+  } catch (err) {
+    console.log('抓取錯誤',error);
+    return;
   }
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i].id + ' ' + data[i].name)
+ }
+ },
 );
+
